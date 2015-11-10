359007561450c094d1a65c19c9ad4608a5888331
Sophia <sofiya.belous@gmail.com>
2015-11-10 16:46:17 +0300

    Added parallax slides on home page

--- SpaDogs/public/js/controllers/MainCtrl.js
+++ SpaDogs/public/js/controllers/MainCtrl.js
@@ -4,20 +4,13 @@ mainModule.controller('MainController', function($scope) {
 	
 	$scope.pageClass = 'page-home';
 	
-	$scope.myInterval = 0;
+	$scope.myInterval = 5000;
 	$scope.noWrapSlides = false;
-	var slides = $scope.slides = [];
-	$scope.addSlide = function() {
-		var newWidth = 500 + slides.length + 1;
-			slides.push({
-				image: '//placekitten.com/' + newWidth + '/300',
-				text: ['More','Extra','Lots of'][slides.length % 3] + ' ' +
-						['Cats', 'Kittys', 'Felines'][slides.length % 3]
-			});
-	};
-	for (var i=0; i<3; i++) {
-	$scope.addSlide();
-	}
+	$scope.slides = [
+		{image:'/uploads/stylePhotos/1440x900_High_resolution_puppy_photograph_406345.jpg', text: 'More Cats'},
+		{image:'/uploads/stylePhotos/white-dog-white-background.jpg', text: 'Extra Kittys'},
+		{image:'/uploads/stylePhotos/Black-Dogs-Puppies-Wallpaper-HD.jpg', text: 'Lots of Felines'}
+	];
 });
 
 mainModule.controller('PetsController', function($scope, $location, Animal) {
