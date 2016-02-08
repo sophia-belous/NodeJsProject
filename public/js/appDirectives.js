a97b2837b416d959ec6e0cdc1d40ae532a32d967
Sophia <sofiya.belous@gmail.com>
2016-02-08 16:08:04 +0300

    Added new directive for smooth scrolling

--- public/js/appDirectives.js
+++ public/js/appDirectives.js
@@ -30,3 +30,48 @@ app.directive('commentList', function() {
 		controller: 'CommentsController'
 	};
 });
+
+app.directive('mySmoothScroll', function($window) {
+	return {
+		restrict: 'A',
+		link: function($scope, element, attrs) {
+			var t = this, h = document.documentElement;
+			element = window;
+				t.rAF = false;
+				t.target = 0;
+				t.scroll = 0;
+				t.animate = function() {
+					t.scroll += (t.target - t.scroll) * 0.1;
+					
+					if (Math.abs(t.scroll.toFixed(5) - t.target) <= 0.47131) {
+						cancelAnimationFrame(t.rAF);
+						t.rAF = false;
+					}
+					if (element == window) scrollTo(0, t.scroll);
+					else element.scrollTop = t.scroll;
+					
+					if (t.rAF) t.rAF = requestAnimationFrame(t.animate);
+				};
+				
+			angular.element($window).bind("scroll", function() {	
+				if (t.rAF) return;
+				
+				t.target = (element == window) ? pageYOffset || h.scrollTop : element.scrollTop;
+				t.scroll = t.target;				
+			});
+						
+			angular.element($window).bind("mousewheel", function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+				
+				var scrollEnd = (element == window) ? h.scrollHeight - h.clientHeight : element.scrollHeight - element.clientHeight;
+				
+				t.target += (e.originalEvent.wheelDelta > 0) ? -70 : 70;
+				if (t.target < 0) t.target = 0;
+				if (t.target > scrollEnd) t.target = scrollEnd;
+				if (!t.rAF) t.rAF = requestAnimationFrame(t.animate);
+			});
+			
+		}
+	};
+});
